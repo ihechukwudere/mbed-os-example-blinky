babe2c2c5c205481d9f3e499d1900fe8efa8131a
Ihechukwudere Okoroego <hechukwudere.okoroego@aalto.fi>
2022-02-15 22:36:23 +0200

    Interruption with buttons

--- main.cpp
+++ main.cpp
@@ -5,21 +5,20 @@
 
 #include "mbed.h"
 
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+#define BLINKING_RATE 500ms
 
+InterruptIn mypin(BUTTON1);
+// Initialise the digital pin LED1 as an output
+DigitalOut led(LED1);
 
-DigitalIn mypin(BUTTON1);
+void button_isr(void) { led = mypin; }
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-    
-    while (true) {
-        led = mypin;
-         printf("mypin has value : %d \n\r", !led.read());
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+int main() {
+    mypin.rise(&button_isr);
+    mypin.fall(&button_isr);
+  while (true) {
+    printf("mypin has value : %d \n\r", !led.read());
+    ThisThread::sleep_for(BLINKING_RATE);
+  }
 }

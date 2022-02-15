48eea1a1a4eafd50d3575a65100cad23377ef438
Ihechukwudere Okoroego <hechukwudere.okoroego@aalto.fi>
2022-02-15 20:32:11 +0200

    Project init and task 1 completed

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+DigitalIn mypin(BUTTON1);
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+    led = !mypin;
+    while (true) {
+        led = !mypin;
+         printf("mypin has value : %d \n\r", led.read());
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
